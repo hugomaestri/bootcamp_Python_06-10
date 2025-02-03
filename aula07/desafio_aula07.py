960fc5fdf07dd7ba2f8fe85fef5c9fdca61ff696
hugoguilhermemaestri <hugo.maestri@gmail.com>
2025-02-03 18:02:29 -0300

    :recycle: refactor: Improve the print message

--- aula07/desafio_aula07.py
+++ aula07/desafio_aula07.py
@@ -1,17 +1,14 @@
 ## Analise de vendas de produtos
-# Ler CSV
-# Processar dados
-# Filtrar produtos en
-# Exibir resultados
 
 from etl import ler_csv, filtrar_produtos_entregues, somar_valores_produtos
 
 path_arquivo = "vendas.csv"
 
 lista_produtos = ler_csv(path_arquivo)
-print(lista_produtos)
+print(f"Lista de produtos: {lista_produtos}")
+
 produtos_entregues = filtrar_produtos_entregues(lista_produtos)
-print(produtos_entregues)
+print(f"\nLista de produtos entregues: {produtos_entregues}")
 
 valor_produtos_entregues = somar_valores_produtos(produtos_entregues)
-print(valor_produtos_entregues)
+print(f"\nO valor dos produtos entregues é de: R$ {valor_produtos_entregues:.2f}")
